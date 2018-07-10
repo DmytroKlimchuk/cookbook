2ad7be129b71bc9eea9f5a79ab3d169878e4885d
Dmytro Klimchuk <Dmytro.Klimchuk@gmail.com>
2018-07-10 19:19:35 +0300

    export router to module

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,29 @@
+import {RouterModule, Routes} from '@angular/router';
+
+import { ShoppingComponent } from './shopping/shopping.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+
+import { NgModule } from '@angular/core';
+
+
+const appRouter: Routes = [
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    {path: 'recipes', component: RecipiesComponent},
+    {path: 'shopping-list', component: ShoppingComponent}
+  ];
+
+export const routerComponents = [
+  RecipiesComponent,
+  ShoppingComponent
+]
+
+  @NgModule({
+    imports: [
+      RouterModule.forRoot(appRouter)
+    ],
+    exports: [
+      RouterModule
+    ]
+  })
+
+  export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { RecipiesComponent } from './recipies/recipies.component';
+
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './header/navigation/navigation.component';
+
 import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
-import { HeaderComponent } from './header/header.component';
+import { RecipesService } from './recipies/recipes.service';
+
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
-import { NavigationComponent } from './header/navigation/navigation.component';
-import { ShoppingComponent } from './shopping/shopping.component';
 import { ShoppingFormComponent } from './shopping/shopping-form/shopping-form.component';
-
-import { RecipesService } from './recipies/recipes.service';
 import { ShoppingService } from './shopping/shopping.service';
 
-const routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipiesComponent},
-  {path: 'shopping-list', component: ShoppingComponent}
-];
+import { AppRoutingModule, routerComponents } from './app-router.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipiesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     HeaderComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     NavigationComponent,
-    ShoppingComponent,
-    ShoppingFormComponent
+    ShoppingFormComponent,
+    ...routerComponents
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [RecipesService, ShoppingService],
   bootstrap: [AppComponent]
